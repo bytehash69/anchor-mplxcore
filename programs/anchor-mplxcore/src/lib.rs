67e985095abba4780f25dc904df0084dceec52b3
bytehash69 <bytehash69@gmail.com>
2025-11-03 00:25:46 +0530

    add: mplxcore program & test

--- programs/anchor-mplxcore/src/lib.rs
+++ programs/anchor-mplxcore/src/lib.rs
@@ -1,16 +1,39 @@
 use anchor_lang::prelude::*;
 
+mod instructions;
+mod state;
+mod error;
+
+use instructions::*;
+// use state::*;
+
 declare_id!("86BV8L9Vf1a2idboxzewZ7K1ybMrTDHip8sZNQUkm3dd");
 
 #[program]
 pub mod anchor_mplxcore {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn whitelist_creator(ctx: Context<WhitelistCreator>) -> Result<()> {
+        ctx.accounts.whitelist_creator()
+    }
+    
+    pub fn create_collection(ctx: Context<CreateCollection>, args: CreateCollectionArgs) -> Result<()> {
+        ctx.accounts.create_collection(args, &ctx.bumps)
+    }
+
+    pub fn mint_nft(ctx: Context<MintNft>) -> Result<()> {
+        ctx.accounts.mint_nft()
+    }
+
+    pub fn freeze_nft(ctx: Context<FreezeNft>) -> Result<()> {
+        ctx.accounts.freeze_nft()
+    }
+
+    pub fn thaw_nft(ctx: Context<ThawNft>) -> Result<()> {
+        ctx.accounts.thaw_nft()
+    }
+
+    pub fn update_nft(ctx: Context<UpdateNft>, new_name: Option<String>, new_uri: Option<String>) -> Result<()> {
+        ctx.accounts.update_nft(new_name, new_uri)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
